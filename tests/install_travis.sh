8c60470281697cab8bd693f5a389a77c42050549
Stephan Seitz <stephan.seitz@fau.de>
2020-03-12 01:41:59 +0100

    Travis: Fix CUDA installation

--- tests/install_travis.sh
+++ tests/install_travis.sh
@@ -48,7 +48,7 @@ sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
-sudo apt-get -y install cuda-$CUDA_VERSION
+sudo apt-get -y install cuda
 sudo ldconfig
 export CUDA_HOME=/usr/local/cuda
 export PATH=/usr/local/cuda/bin:$PATH
